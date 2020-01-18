907849408c6496e2177c1f15dfbb38ac09c3ef66
Tomi Poveda <tpovedatd@gmail.com>
2020-01-18 13:32:07 +0100

    Added main UI for shots publisher tool

--- artellapipe/tools/sequencespublisher/core/sequencespublisher.py
+++ artellapipe/tools/sequencespublisher/core/sequencespublisher.py
@@ -12,10 +12,26 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
+from tpQtLib.widgets import stack
+
 import artellapipe
 
 
-class SequencesPublisher(artellapipe.Tool, object):
+class SequencesPublisher(artellapipe.PyblishTool, object):
 
     def __init__(self, project, config):
         super(SequencesPublisher, self).__init__(project=project, config=config)
+
+    def ui(self):
+        super(SequencesPublisher, self).ui()
+
+        self._stack = stack.SlidingStackedWidget()
+        self.main_layout.addWidget(self._stack)
+
+        self._sequences_viewer = artellapipe.SequencesViewer(project=self._project)
+        self._stack.addWidget(self._sequences_viewer)
+
+    def post_attacher_set(self):
+        super(SequencesPublisher, self).post_attacher_set()
+
+        self._stack.addWidget(self._pyblish_window)
